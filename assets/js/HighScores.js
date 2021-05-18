0775ac33e510434d8bdcb16bd85b853ee4eecd5e
Jason Bailey <jmbailey816@gmail.com>
2021-05-18 16:13:56 -0400

    Code cleanup and commenting

--- assets/js/HighScores.js
+++ assets/js/HighScores.js
@@ -2,25 +2,29 @@ var highScore = document.querySelector("#highScore");
 var clear = document.querySelector("#clear");
 var goBack = document.querySelector("#goBack");
 
-// how to sort the scores high to low rather than chronological
+// how to sort the scores high to low rather than chronological -
 var allScores = localStorage.getItem("allScores");
 allScores = JSON.parse(allScores);
 
 if (allScores !== null) {
   for (var i = 0; i < allScores.length; i++) {
     var createLi = document.createElement("li");
-    createLi.textContent = allScores[i].initials + " -- " + allScores[i].score;
+    createLi.textContent = allScores[i].score + " -- " + allScores[i].initials;
     highScore.appendChild(createLi);
   }
 }
 
 // back to home button
 goBack.addEventListener("click", function () {
-  window.location.replace("./index.html");
+  window.location.replace("./index.html"); // takes user back to start page
 });
 
 // clear scores button
 clear.addEventListener("click", function () {
-  localStorage.clear();
+  localStorage.clear(); 
   location.reload();
 });
+
+console.log(allScores);
+console.log(highScore);
+console.log(allScores[2]);
